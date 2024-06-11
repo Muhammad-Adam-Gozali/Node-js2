78510b0654ed45acc9232529983fe672b93e0e81
Muhammad Adam Gozali <muhammadadamgozali@gmail.com>
2024-06-12 00:38:30 +0700

    Tugas 2 Node.js

---
+++ Readme.md
@@ -0,0 +1,8 @@
+#install
+npm install -g ts-node typescript
+
+#run project
+ts-node readFile.ts
+ts-node server.ts
+ts-node useMath.ts
+
---
+++ useMath.ts
@@ -0,0 +1,6 @@
+import { add, subtract, multiply, divide } from './mathOperations';
+
+console.log('Add: ', add(2, 3));
+console.log('Subtract: ', subtract(5, 2));
+console.log('Multiply: ', multiply(3, 4));
+console.log('Divide: ', divide(10, 2));
